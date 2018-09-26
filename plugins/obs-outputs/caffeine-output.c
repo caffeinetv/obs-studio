27732e5d98340859c9c1708decae9c0220b3331e
Matthew Orlando <cogwheel@caffeine.tv>
2018-09-25 17:54:45 -0700

    obs-outputs: Add stubs for caffeine.tv support

---
+++ plugins/obs-outputs/caffeine-output.c
@@ -0,0 +1,107 @@
+#include <obs.h>
+#include <obs-module.h>
+
+#include "caffeine.h"
+
+#define do_log(level, format, ...) \
+	blog(level, "[caffeine output: '%s'] " format, \
+			obs_output_get_name(stream->output), ##__VA_ARGS__)
+
+#define warn(format, ...)  do_log(LOG_WARNING, format, ##__VA_ARGS__)
+#define info(format, ...)  do_log(LOG_INFO, format, ##__VA_ARGS__)
+
+struct caffeine_output
+{
+	obs_output_t *output;
+};
+
+static const char *caffeine_get_name(void *data)
+{
+	UNUSED_PARAMETER(data);
+
+	blog(LOG_INFO, "caffeine_get_name");
+
+	return obs_module_text("CaffeineOutput"); // TODO localize
+}
+
+static void *caffeine_create(obs_data_t *settings, obs_output_t *output)
+{
+	UNUSED_PARAMETER(settings);
+
+	struct caffeine_output *stream = bzalloc(sizeof(struct caffeine_output));
+	stream->output = output;
+
+	info("caffeine_create");
+
+	info("TestStub: %d", (int)TestStub()); /* temp - make sure we can call into caffeine-rtc */
+	/* TODO */
+
+	return stream;
+}
+
+static void caffeine_destroy(void *data)
+{
+	struct caffeine_output *stream = data;
+
+	info("caffeine_destroy");
+	/* TODO */
+
+	bfree(data);
+}
+
+static bool caffeine_start(void *data)
+{
+	struct caffeine_output *stream = data;
+
+	info("caffeine_start");
+	/* TODO */
+
+	return true;
+}
+
+static void caffeine_stop(void *data, uint64_t ts)
+{
+	UNUSED_PARAMETER(ts);
+
+	struct caffeine_output *stream = data;
+
+	info("caffeine_stop");
+	/* TODO */
+}
+
+static void caffeine_raw_video(void *data, struct video_data *frame)
+{
+	UNUSED_PARAMETER(frame);
+
+	struct caffeine_output *stream = data;
+
+	info("caffeine_raw_video");
+	/* TODO */
+}
+
+static void caffeine_raw_audio(void *data, struct audio_data *frames)
+{
+	UNUSED_PARAMETER(frames);
+
+	struct caffeine_output *stream = data;
+
+	info("caffeine_raw_audio");
+	/* TODO */
+}
+
+struct obs_output_info caffeine_output_info = {
+	.id        = "caffeine_output",
+	.flags     = OBS_OUTPUT_AV | OBS_OUTPUT_SERVICE,
+	.get_name  = caffeine_get_name,
+	.create    = caffeine_create,
+	.destroy   = caffeine_destroy,
+	.start     = caffeine_start,
+	.stop      = caffeine_stop,
+	.raw_video = caffeine_raw_video,
+	.raw_audio = caffeine_raw_audio,
+
+	/* These are supposed to be optional for non-encoded streams but stream
+	   fails to start if they're omitted */
+	.encoded_video_codecs = "h264",
+	.encoded_audio_codecs = "opus",
+};
