9eb01d0baa1b1f3c9f4f90935a1130ae61388dba
Matthew Orlando <cogwheel@caffeine.tv>
2019-04-17 17:10:32 -0700

    UI, plugins: Add Caffeine.tv output

---
+++ UI/window-caffeine.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "ui_CaffeinePanel.h"
+#include <QDialog>
+#include <QPointer>
+#include <QDockWidget>
+#include <QBoxLayout>
+#include <QLayout>
+
+#include <memory>
+
+#include "window-dock.hpp"
+#include "window-basic-main.hpp"
+#include "auth-caffeine.hpp"
+
+#include <caffeine.h>
+
+Q_DECLARE_METATYPE(caff_Rating);
+
+class CaffeineInfoPanel : public OBSDock {
+	Q_OBJECT
+
+private:
+	CaffeineAuth* owner;
+	std::shared_ptr<Ui::CaffeinePanel> ui;
+	caff_InstanceHandle caffeineInstance;
+
+	void registerDockWidget();
+
+public slots:
+	void updateClicked(bool);
+
+	void viewOnWebClicked(bool);
+
+public:
+	CaffeineInfoPanel(CaffeineAuth* owner, caff_InstanceHandle instance);
+	virtual ~CaffeineInfoPanel() override;
+
+	std::string getTitle();
+	void setTitle(std::string title);
+
+	caff_Rating getRating();
+	void setRating(caff_Rating rating);
+};
---
+++ plugins/caffeine/README.rst
@@ -0,0 +1,10 @@
+Caffeine Plugin
+===============
+
+Building
+--------
+
+1. Download the libcaffeine CPack package or check out [libcaffeine](https://github.com/caffeinetv/libcaffeine) and build it.
+2. Set LIBCAFFEINE_DIR to either your built project directory or to where you extracted the libcaffeine CPack package.
+2. Configure
+3. Generate
