42d640b8ebd0229b0160d3a43d6cd371b909d510
turbodavidson <43543791+turbodavidson@users.noreply.github.com>
2021-02-18 18:25:05 -0500

    COBS-61 (#96)
    * Added sample logger
    
    * Removed windows header
    
    * Updated CMakeLists.txt
    
    * Added video frame window based on latest processed audio sample
    
    * Expanded sync window to +-3 Frames at 30fps. Seems to help dropping unneccisarily for small variations
    
    * Enhanced logging, set av timestamp to sample range center, drop video packets out of range, stall video pipeline if av desync causes drop
    
    * Format code
    
    * Updated the functions to work with macos
    
    * Formated the code
    
    * Fixed build error on windows
    
    * Added CAFFEINE_COBS_TEST_FRAME_DROP_PERCENT for dropping testing and dropping a percentage of frames
    
    * clang-format ran
    
    * added caff_min and caff_max for xp compatibility
    
    * clang-format again (caff_min name change put line just over length limit)
    
    Co-authored-by: prateekshadas <61755734+prateekshadas@users.noreply.github.com>

---
+++ plugins/caffeine/caffeine-sample-logger.c
@@ -0,0 +1,104 @@
+#include "caffeine-sample-logger.h"
+#include <string.h>
+#include <stdarg.h>
+
+void caffeine_sample_logger_log(caffeine_sample_logger_t *lpsl,
+				const char *format, ...)
+{
+	if (false == lpsl->is_ok) {
+		return;
+	}
+	va_list args;
+	memset(lpsl->line_buff, 0, sizeof(lpsl->line_buff));
+	va_start(args, format);
+	int num_chars = vsnprintf(lpsl->line_buff, sizeof(lpsl->line_buff),
+				  format, args);
+	va_end(args);
+	FILE *file_handle = fopen(lpsl->file_name, "a+b");
+	if (file_handle) {
+		fwrite(lpsl->line_buff, 1, num_chars, file_handle);
+		fclose(file_handle);
+	}
+}
+
+bool caffeine_sample_logger_init(caffeine_sample_logger_t *lpsl,
+				 const char *file_name)
+{
+	memset(lpsl, 0, sizeof(caffeine_sample_logger_t));
+	strncpy(lpsl->file_name, file_name, sizeof(lpsl->file_name));
+	FILE *file_handle = fopen(lpsl->file_name, "w+b");
+	if (file_handle) {
+		fclose(file_handle);
+		lpsl->is_ok = true;
+	}
+	caffeine_sample_logger_log(lpsl,
+				   "sample"
+				   ",sent to libcaffeine"
+				   ",reason"
+				   ",wall time (ms)"
+				   ",wall time delta (ms)"
+				   ",sample obs timestamp (ms)"
+				   ",sample obs timestamp delta (ms)"
+				   ",sample obs timestamp delta from pair (ms)"
+				   ",caffeine func time (ms)"
+				   ",caffeine func time delta (ms)"
+				   ",obs app time (ms)"
+				   ",obs app time delta (ms)"
+				   "\r\n");
+	return lpsl->is_ok;
+}
+
+void caffeine_sample_logger_log_sample(
+	caffeine_sample_logger_t *lpsl, bool sample_sent_to_libcaffeine,
+	const char *reason_not_sent_to_libcaffeine, uint64_t wall_time_ns,
+	uint64_t sample_obs_timestamp_ns, uint64_t pair_obs_last_timestamp_ns,
+	uint64_t caffeine_func_time_ns, uint64_t obs_app_time_ns)
+{
+	double wall_time_ms = (double)wall_time_ns / 1000000.0;
+	double sample_obs_timestamp_ms =
+		(double)sample_obs_timestamp_ns / 1000000.0;
+	double caffeine_func_time_ms =
+		(double)caffeine_func_time_ns / 1000000.0;
+	double obs_app_time_ms = (double)obs_app_time_ns / 1000000.0;
+	const char *sample_sent_yn = (true == sample_sent_to_libcaffeine) ? "Y"
+									  : "N";
+	double pair_obs_last_timestamp_ms =
+		(double)pair_obs_last_timestamp_ns / 1000000.0;
+	double pair_obs_timestamp_delta_from_pair_ms =
+		sample_obs_timestamp_ms - pair_obs_last_timestamp_ms;
+
+	if (lpsl->sample_cnt == 0) {
+		caffeine_sample_logger_log(
+			lpsl, "%d,%s,%s,%0.3f,0,%0.3f,0,0,%0.3f,0,%0.3f,0\r\n",
+			lpsl->sample_cnt, sample_sent_yn,
+			reason_not_sent_to_libcaffeine, wall_time_ms,
+			sample_obs_timestamp_ms, caffeine_func_time_ms,
+			obs_app_time_ms);
+	} else {
+		double wall_time_ms_delta =
+			wall_time_ms - lpsl->prev_wall_time_ms;
+		double sample_obs_timestamp_ms_delta =
+			sample_obs_timestamp_ms -
+			lpsl->prev_sample_obs_timestamp_ms;
+		double caffeine_func_time_ms_delta =
+			caffeine_func_time_ms -
+			lpsl->prev_caffeine_func_time_ms;
+		double obs_app_time_ms_delta =
+			obs_app_time_ms - lpsl->prev_obs_app_time_ms;
+		caffeine_sample_logger_log(
+			lpsl,
+			"%d,%s,%s,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f\r\n",
+			lpsl->sample_cnt, sample_sent_yn,
+			reason_not_sent_to_libcaffeine, wall_time_ms,
+			wall_time_ms_delta, sample_obs_timestamp_ms,
+			sample_obs_timestamp_ms_delta,
+			pair_obs_timestamp_delta_from_pair_ms,
+			caffeine_func_time_ms, caffeine_func_time_ms_delta,
+			obs_app_time_ms, obs_app_time_ms_delta);
+	}
+	lpsl->prev_wall_time_ms = wall_time_ms;
+	lpsl->prev_sample_obs_timestamp_ms = sample_obs_timestamp_ms;
+	lpsl->prev_caffeine_func_time_ms = caffeine_func_time_ms;
+	lpsl->prev_obs_app_time_ms = obs_app_time_ms;
+	lpsl->sample_cnt++;
+}
